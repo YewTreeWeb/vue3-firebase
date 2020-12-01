376237d92db03e0bd27b57b196f55b50630524fc
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-12-01 16:59:50 +0000

    added new component and used

---
+++ src/components/VueThree.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>{{ title }}</h2>
+  <input type="text" ref="name" />
+  <button @click="handleClick">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: "VueThree",
+  data() {
+    return {
+      title: "My First Component with Vue 3",
+    };
+  },
+  methods: {
+    handleClick() {
+      // $refs give references to DOM elements
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+h2 {
+  margin: 40px 0 0;
+}
+</style>
